88eb4ddad9246b21e2a806b4cd39538b994a2cc8
Marcelo Ribas <57764227+devmarceloribas@users.noreply.github.com>
2019-12-30 23:36:30 +0000

    Add the database e configured ejs to acess the database

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// Express
 const express = require('express')
 const app = express()
+
+// SQL Lite
+const sqlite = require('sqlite')
+const dbConn = sqlite.open('jobiFy.sqlite', {Promise})
+
 
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-
-
 // Routers
-app.get('/', (req, res) => {
-  res.render('home')
+app.get('/', async(req, res) => {
+  const db = await dbConn
+  const categorias = await db.all('select * from categorias')
+  res.render('home', {
+    categorias
+  })
 })
 
 app.get('/vaga', (req, res) => {
@@ -20,11 +28,22 @@ app.get('/aboutme', (req, res) => {
   res.render('aboutme')
 })
 
-// Vagas
-
-// Categorias
-
 // Database
+const init = async() => {
+  const db = await dbConn
+  await db.run('create table if not exists categorias (id integer primary key, categoria text)')
+  //const categoria = 'Engeneering Team'
+  //await db.run(`insert into categorias (categoria) values ('${categoria}');`)
+  
+  //const categoria2 = 'Marketing Team'
+  //await db.run(`insert into categorias (categoria) values ('${categoria2}');`)
+}
+
+init()
+
+// Oportunitys
+
+// Category
 
 
 app.listen(8080, (err) => {
